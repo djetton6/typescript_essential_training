40b30094df4243f97006efda01afae194a951edd
Desmend Jetton <desmendjetton@gmail.com>
2023-01-19 18:02:49 -0600

    learning more from typescript

--- src/demo.ts
+++ src/demo.ts
@@ -1,5 +1,30 @@
-class Contact {
-    id;
-    name;
-    birthDate;
+interface Contact extends Address {
+    id: number;
+    name: string;
+    birthDate: Date;
+    status: ContactStatus;
 }
+
+
+interface Address {
+    line1: string
+    line2: string
+}
+
+enum ContactStatus {
+    Active = 'active',
+    Offline = "offline",
+
+}
+// Interface is used for typechecking(only used at compile time)
+
+let primaryContact: Contact = {
+    birthDate: new Date("01-01-1939"),
+    id: 12345,
+    name: "Testing 123",
+    line1: "Cherry Tree",
+    line2: "Chicago, IL",
+    status: ContactStatus.Offline,
+}
+
+type ContactName = string
